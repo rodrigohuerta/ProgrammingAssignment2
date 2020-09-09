62ae76f141590a2403a5b89648e1dafe76e45967
Rodrigo Huerta <rodrigo.huerta.gdv@gmail.com>
2020-09-08 22:16:55 -0500

    Testing matrix function

--- cachematrix.R
+++ cachematrix.R
@@ -4,22 +4,98 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-  m<-NULL
+  inversa<-NULL
   set<-function(y){
     x<<-y
-    m<<-NULL
+    inversa<<-NULL
   }
   get<-function() x
-  setinverse<-function(solve) m<<-solve
-  getinverse<-function() m
+  setinversa<-function(inv) inversa<<-inv
+  getinversa<-function(){
+    solve(x)
+  }
   list(set=set, get=get,
-       setinverse=setinverse,
-       getinverse=getinverse )
+       setinversa=setinversa,
+       getinversa=getinversa)
 }
 
+cacheSolve <- function(x, ...) {
+  inversa <- x$getinversa()
+  if(!is.null(inversa)) {
+    message("getting cached data")
+    return(inversa)
+  }
+  data <- inversa$get()
+  inversa <- solve(data, ...)
+  x$setinversa(inversa)
+  inversa
+}
+
+
+# Testing makeCachematrix function
+
+mimatriz<-matrix(c(4,2,7,6),2,2)
+
+print(mimatriz)
+
+solve(mimatriz)
+
+makeCacheMatrix(mimatriz)
+
+solve(mimatriz)
+
+# Using Makevector
+
+rm(list = ls())
+
+
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
+}
+
+cachemean <- function(x, ...) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
+}
+
+# Testing cachemean and cmakevector functions
+
+mivector<-c(1:100)
+
+makeVector(mivector)
+cachemean(mivector)
+
+# Error produced in cachemean function: $ operator is invalid for atomic vectors
+
+cachemean1 <- function(x, ...) {
+  m <- getmean(x)
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- get(x)
+  m <- mean(data, ...)
+}
+
+cachemean1(mivector)
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+
