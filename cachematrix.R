2c2b37976a2867097989d41a3312de356f46a76d
Rodrigo Huerta <rodrigo.huerta.gdv@gmail.com>
2020-09-09 21:46:10 -0500

    Commenting tests

--- cachematrix.R
+++ cachematrix.R
@@ -6,6 +6,8 @@
 
 ## Write a short comment describing this function
 
+## makeCacheMatrix has a matrix as an input and a list as an output. chaceSolve will subset this list
+
 makeCacheMatrix <- function(x = matrix()) {
   inversa<-NULL
   set<-function(y){
@@ -20,6 +22,11 @@ makeCacheMatrix <- function(x = matrix()) {
        getinversa=getinversa)
 }
 
+## Cachesolve receives the output of makecachematrix and outputs the inverse of an invertible matrix
+
+## When the matrix has been previously inverted, cacheSolve skips the inverting process and returns the previous result
+## When the matrix has not been inverted, cacheSolve computes the inverse of the matrix
+
 cacheSolve <- function(x, ...) {
   inversa <- x$getinversa()
   if(!is.null(inversa)) {
@@ -32,124 +39,27 @@ cacheSolve <- function(x, ...) {
   inversa
 }
 
-test4<-makeCacheMatrix(mimatriz)
-
-test4$get()
-test4$getinversa()
-
-test5<- cacheSolve(test4)
-
-test5
 
 # Testing makeCachematrix function
 
-## Creating and solving example matrix
+## Creating and solving an invertible matrix
 
-mimatriz<-matrix(c(4,2,7,6),2,2)
+#mimatriz<-matrix(c(4,2,7,6),2,2)
 
-is.matrix(mimatriz)
+#is.matrix(mimatriz)
 
-solve(mimatriz)
+#solve(mimatriz)
 
 ## Using function to get matrix and its inverse
 
-test<-cacheSolve(mimatriz)
+#test1<-makeCacheMatrix(mimatriz)
 
-test$get()
-  
-test$getinversa()
+#test1$get()
+#test1$getinversa()
+
+#test2<- cacheSolve(test1)
+
+#test2
 
 ## The function is able to retrieve the inverse 
 
-# Testing cacheSolve function
-
-## We use the same example
-
-test2<-cacheSolve(matrix(c(4,2,7,6),2,2))
-
-
-cacheSolve1 <- function(x, ...) {
-  inversa <- x[[getinversa()]]
-  if(!is.null(inversa)) {
-    message("getting cached data")
-    return(inversa)
-  }
-  data <- x$get()
-  inversa <- solve(data, ...)
-  x$setinversa(inversa)
-  inversa
-}
-
-test3<-cacheSolve1(matrix(c(4,2,7,6),2,2))
-
-
-# Using Makevector
-
-rm(list = ls())
-
-
-makeVector <- function(x = numeric()) {
-  m <- NULL
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
-  }
-  get <- function() x
-  setmean <- function(mean) m <<- mean
-  getmean <- function() m
-  list(set = set, get = get,
-       setmean = setmean,
-       getmean = getmean)
-}
-
-cachemean <- function(x, ...) {
-  m <- x$getmean()
-  if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
-  }
-  data <- x$get()
-  m <- mean(data, ...)
-  x$setmean(m)
-  m
-}
-
-# Testing cachemean and cmakevector functions
-
-mivector<-c(1:100)
-
-makeVector(mivector)
-cachemean(mivector)
-
-# Error produced in cachemean function: $ operator is invalid for atomic vectors
-
-cachemean1 <- function(x, ...) {
-  m <- getmean(x)
-  if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
-  }
-  data <- get(x)
-  m <- mean(data, ...)
-}
-
-cachemean1(mivector)
-
-## Write a short comment describing this function
-
-
-makeCacheMatrix <- function(x = matrix()) {
-  inversa<-NULL
-  set<-function(y){
-    x<<-y
-    inversa<<-NULL
-  }
-  get<-function() x
-  setinversa<-function(inv) inversa<<-inv
-  getinversa<-function(){
-    solve(x)
-  }
-  list(set=set, get=get,
-       setinversa=setinversa,
-       getinversa=getinversa)
-}
